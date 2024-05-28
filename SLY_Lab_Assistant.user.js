119ab690693d8888fc80d800c89b65e5f454df43
kryptokris71 <kryptokris.c@gmail.com>
2024-05-28 03:45:12 +0200

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -4,7 +4,7 @@
 // @version      0.4.3
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
-// @match        https://*.labs.staratlas.com/
+// @match        https://*.base.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
 // @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1/main/anchor-browserified.js
 // @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1/main/buffer-browserified.js
