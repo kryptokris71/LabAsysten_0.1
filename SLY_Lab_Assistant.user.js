12fcb18557dae60e19168cdba168cb3210fa459b
kryptokris71 <kryptokris.c@gmail.com>
2024-05-28 03:24:41 +0200

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,7 +6,7 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1/main/anchor-browserified.js
 // @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1/main/buffer-browserified.js
 // @require      https://raw.githubusercontent.com/kryptokris71/LabAsystent_0.1/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
